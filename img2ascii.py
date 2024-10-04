a1297d045f1e1f7741e4be62f56b90c799b695b0
paopeaw <paopeaw8246@gmail.com>
2024-10-04 23:25:47 +0700

    intregrate pixel mode into main file

--- img2ascii.py
+++ img2ascii.py
@@ -5,17 +5,24 @@ BLOCKS_CHARS = ["\u2588"]
 BRAILLE_CHARS = ["\u28FF"]
 
 # Resize image while maintaining aspect ratio
-def resize_image(image, new_width=100):
+def resize_image(image, new_width=100,scale=2.1):
     width, height = image.size
-    aspect_ratio = height / width / 2.1  # Adjusting ratio for ASCII look
+    aspect_ratio = height / width / scale  # Adjusting ratio for ASCII look
+    if(new_width % 2 != 0 ):
+        new_width-=1
     new_height = int(aspect_ratio * new_width)
+    if(new_height% 2 != 0 ):
+        new_height-=1
     return image.resize((new_width, new_height))
+
+def upper_lower_pixels_to_ascii(up,lp):
+    ascii = "[38;2;{lr};{lg};{lb};48;2;{ur};{ug};{ub}m\u2584".format(ur=up[0],ug=up[1],ub=up[2],lr=lp[0],lg=lp[1],lb=lp[2])
+    return ascii
 
 def pixels_to_ascii(r,g,b, select_pattern):
     lum = (0.2126 * r + 0.7152 *g+ 0.0722 * b)/255  # 0-1
     bin_size = len(select_pattern)
     word = select_pattern[round(lum * (bin_size-1))]
-    # word = "\u28FF"
     ascii = "[38;2;{0};{1};{2}m{3}".format(r,g,b,word)
     return ascii
 
@@ -39,19 +46,38 @@ def image_to_ascii(image_path,store_path, new_width=100, pattern='a'):
         case _:
             select_pattern = ASCII_CHARS
 
-    image = resize_image(image, new_width)
-    rgbImage = image.convert('RGB')
-    size = rgbImage.size;
-    for i in range(size[1]):
-        ascii_str = ""
-        for j in range(size[0]):
-            r,g,b = rgbImage.getpixel((j,i));
-            ascii = pixels_to_ascii(r,g,b, select_pattern)
-            ascii_str += ascii;
-        print(ascii_str)
-        ascii_str += "\n"
-        f.write(ascii_str)
-    f.close()
+    if(pattern != 'p'):
+        image = resize_image(image, new_width)
+        rgbImage = image.convert('RGB')
+        size = rgbImage.size;
+        for i in range(size[1]):
+            ascii_str = ""
+            for j in range(size[0]):
+                r,g,b = rgbImage.getpixel((j,i));
+                ascii = pixels_to_ascii(r,g,b, select_pattern)
+                ascii_str += ascii;
+            print(ascii_str)
+            ascii_str += "[m"
+            ascii_str += "\n"
+            f.write(ascii_str)
+        f.close()
+    else:
+        image = resize_image(image, new_width,1)
+        rgbImage = image.convert('RGB')
+        size = rgbImage.size;
+        for j in range(0,size[1],2):
+            ascii_str = ""
+            for i in range(size[0]):
+                up = rgbImage.getpixel((i,j));
+                lp = rgbImage.getpixel((i,j+1));
+                ascii = upper_lower_pixels_to_ascii(up,lp)
+                ascii_str += ascii;
+            print(ascii_str)
+            ascii_str += "[m"
+            ascii_str += "\n"
+            f.write(ascii_str)
+        f.close()
+
 
 
 if __name__ == "__main__":
